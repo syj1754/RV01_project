d78f4ea72f4c4125b12114ec069fc4ec78375ba8
ThomasJang <thomasjang97@hotmail.com>
2021-12-09 09:31:12 +0100

    add Interface scenes

---
+++ Assets/StartingInterface/Scripts/StartingMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class StartingMenu : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        EditorApplication.Exit(0);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("StartingScene");
+    }
+
+    public void Pause()
+    {
+        SceneManager.LoadScene("PauseScene");
+    }
+}
